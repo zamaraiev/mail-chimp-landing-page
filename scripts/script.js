757e3c8de50e4eb31174dba5725eb0abd99f8b8a
zamaraiev <zamaraiev14@yahoo.com>
2024-06-14 17:20:02 +0200

    Update #9

--- scripts/script.js
+++ scripts/script.js
@@ -13,9 +13,11 @@ document.querySelectorAll('.additional-info__item-title').forEach(item => {
     document.addEventListener("DOMContentLoaded", function() {
         const toggleButton = document.querySelector('.header__toggle');
         const menu = document.querySelector('.header__menu');
+        const controls = document.querySelector('.header__controls');
 
         toggleButton.addEventListener('click', function() {
             menu.classList.toggle('header__menu--active');
+            controls.classList.toggle('header__menu--active');
         });
     });
 
