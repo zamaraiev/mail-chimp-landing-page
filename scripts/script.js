976395b4af986f731a814a95da10a660d90e0654
zamaraiev <zamaraiev14@yahoo.com>
2024-12-30 00:18:24 +0100

    Fix some issues

--- scripts/script.js
+++ scripts/script.js
@@ -12,34 +12,39 @@ document.querySelectorAll('.additional-info__item-title').forEach(item => {
 });
 
 
-    document.addEventListener("DOMContentLoaded", function() {
-        const toggleButton = document.querySelector('.header__toggle');
-        const menu = document.querySelector('.header__menu');
-        const controls = document.querySelector('.header__controls');
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.querySelector('.header__toggle');
+    const menu = document.querySelector('.header__menu');
+    const controls = document.querySelector('.header__controls');
 
-        toggleButton.addEventListener('click', function() {
-            menu.classList.toggle('header__menu--active');
-            controls.classList.toggle('header__menu--active');
-        });
+    toggleButton.addEventListener('click', function() {
+        menu.classList.toggle('header__menu--active');
+        controls.classList.toggle('header__menu--active');
     });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-    const signupButton = document.getElementById('sign-up-button');
+    const signupButtons = document.getElementsByClassName('sign-up-button'); // HTMLCollection
     const popup = document.getElementById('signup-popup');
     const closeButton = document.getElementById('close-popup');
     
-        signupButton.addEventListener('click', () => {
+    console.log(signupButtons);
+    
+    for (const button of signupButtons) {
+        button.addEventListener('click', () => {
             popup.style.display = 'flex';
         });
+    }
     
-        closeButton.addEventListener('click', () => {
-            popup.style.display = 'none';
-        });
-    
-        window.addEventListener('click', (e) => {
-            if (e.target === popup) {
-                popup.style.display = 'none';
-            }
-        });
+    closeButton.addEventListener('click', () => {
+        popup.style.display = 'none';
     });
+    
+    window.addEventListener('click', (e) => {
+        if (e.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+    
     
